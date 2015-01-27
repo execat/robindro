b02b4f4df144015cd6c06c5c4a81a66b6d36b326
Anuj More <anujmorex@gmail.com>
2015-01-28 01:33:52 +0530

    Adds translation marker

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 import sqlite3
 import time
 import string
+import code
 from bs4 import BeautifulSoup
 
 def song_list_spider(letters):
@@ -9,10 +10,12 @@ def song_list_spider(letters):
     conn = sqlite3.connect("geetabitan.db")
     c = conn.cursor()
     create_string = '''
-            CREATE TABLE geetabitan_links
-            (song_name string, song_link string, created_at datetime)
+            CREATE TABLE IF NOT EXISTS geetabitan_links
+            (song_name string, song_link string, translation_available boolean, created_at datetime, updated_at)
     '''
     c.execute(create_string)
+    index_string = "CREATE UNIQUE INDEX IF NOT EXISTS UniqueSongLinks ON geetabitan_links (song_link)"
+    c.execute(index_string)
 
     for letter in letters:
         base_url = "http://www.geetabitan.com/lyrics/" + str(letter)
@@ -28,7 +31,7 @@ def song_list_spider(letters):
             page_name = link.get_text()
             page_link = base_url + "/" + link.get('href')
             insert_string = '''
-                    INSERT INTO geetabitan_links VALUES
+                    INSERT INTO geetabitan_links (song_name, song_link, created_at) VALUES
                     ('%s', '%s', %s)
             ''' %(page_name, page_link, time.time())
             print(insert_string)
@@ -36,7 +39,37 @@ def song_list_spider(letters):
     conn.commit()
     conn.close()
 
+def eng_translation_spider(letters):
+    # Create database and table
+    conn = sqlite3.connect("geetabitan.db")
+    c = conn.cursor()
+
+    for letter in letters:
+        base_url = "http://www.geetabitan.com/lyrics"
+        url = base_url + "/eng-trans-" + letter + ".html"
+        source_code = requests.get(url)
+        if source_code.status_code != 200:
+            continue
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+
+        subpage = soup.select("#wrapper > p > a")
+        for link in subpage:
+            page_name = link.get_text()
+            page_link = base_url + "/" + link.get('href')
+            insert_string = """
+                    UPDATE geetabitan_links
+                    SET translation_available = '%s', updated_at = %s
+                    WHERE song_link = '%s'
+            """ %(True, time.time(), page_link)
+            print(insert_string)
+            c.execute(insert_string)
+    conn.commit()
+    conn.close()
+
 song_list_spider(list(string.ascii_uppercase))
+# song_list_spider(['A'])
+eng_translation_spider(list(string.ascii_uppercase))
 
 
 """
