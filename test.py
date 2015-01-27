98bbd2ab69cb260f0281473848c7befcc7ff919e
Anuj More <anujmorex@gmail.com>
2015-01-27 15:24:59 +0530

    (wip)

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
 import requests
 import sqlite3
+import time
+import string
 from bs4 import BeautifulSoup
 
 def song_list_spider(letters):
+    # Create database and table
     conn = sqlite3.connect("testing.db")
+    c = conn.cursor()
+    create_string = '''
+            CREATE TABLE geetabitan_links
+            (song_name string, song_link string, created_at datetime)
+    '''
+    c.execute(create_string)
 
-    letter = letters[0]
-    while page < len(letters):
+    for letter in letters:
         base_url = "http://www.geetabitan.com/lyrics/" + str(letter)
         url = base_url + "/song-list.html"
         source_code = requests.get(url)
+        if source_code.status_code != 200:
+            continue
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
 
@@ -17,8 +27,16 @@ def song_list_spider(letters):
         for link in subpage.find_all("a"):
             page_name = link.get_text()
             page_link = base_url + "/" + link.get('href')
+            insert_string = '''
+                    INSERT INTO geetabitan_links VALUES
+                    ('%s', '%s', %s)
+            ''' %(page_name, page_link, time.time())
+            print(insert_string)
+            c.execute(insert_string)
+    conn.commit()
+    conn.close()
 
-
+song_list_spider(list(string.ascii_uppercase))
 
 
 """
