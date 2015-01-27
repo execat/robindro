d9dcc6f3e5de8fa8268bed395a726d07f11a92f7
Anuj More <anujmorex@gmail.com>
2015-01-27 14:16:11 +0530

    (wip)

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 import requests
+import sqlite3
 from bs4 import BeautifulSoup
 
 def song_list_spider(letters):
+    conn = sqlite3.connect("testing.db")
+
     letter = letters[0]
     while page < len(letters):
-        url = "http://www.geetabitan.com/lyrics/" + str(letter) + "/song-list.html"
+        base_url = "http://www.geetabitan.com/lyrics/" + str(letter)
+        url = base_url + "/song-list.html"
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+
+        subpage = soup.find("div", class_="lyricsname")
+        for link in subpage.find_all("a"):
+            page_name = link.get_text()
+            page_link = base_url + "/" + link.get('href')
+
 
 
 
