7acf3ca738a31092390e952aee43f06ea1c56078
Anuj More <anujmorex@gmail.com>
2015-01-30 02:08:14 +0530

    Flake

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 
 import os
 import setuptools
-import sys
+# import sys
 
 version_file_path = os.path.join(
-        os.path.dirname(__file__),
-        'robindro',
-        '_version.py'
-        )
+    os.path.dirname(__file__),
+    'robindro',
+    '_version.py'
+    )
+
 with open(version_file_path, 'r') as file_pointer:
     file_contents_string = file_pointer.read()
 local_dict = {}
@@ -23,50 +24,36 @@ long_description = '1'
 long_description += ' 2'
 long_description += ' 3.'
 
-author = [
-        'Anuj More',
-        ]
+author = ['Anuj More']
 author = ', '.join(author)
 
-author_email = [
-        'anujmorex+robindro@gmail.com',
-        ]
+author_email = ['anujmorex+robindro@gmail.com']
 author_email = ', '.join(author_email)
 
-keywords = [
-        'scraper',
-        'crawler',
-        'translation',
-        'literature',
-        ]
+keywords = ['scraper', 'crawler', 'translation', 'literature']
 keywords = ', '.join(keywords)
 
-install_requires = [
-        'pytest',
-        'nose',
-        'beautifulsoup4',
-        'requests',
-        ]
+install_requires = ['pytest', 'nose', 'beautifulsoup4', 'requests']
 
 setuptools.setup(
-        author=author,
-        author_email=author_email,
-        description=description,
-        entry_points={
-            'console_scripts': [
-                'robindro = bin.robindro:run_robindro',
-                ]
-            },
-        include_package_data=True,
-        install_requires=install_requires,
-        keywords=keywords,
-        license='GPL',
-        long_description=long_description,
-        name='Robindro',
-        packages=(
-            'robindro',
-            ),
-        platforms='Any',
-        url='http://github.com/execat/robindro',
-        version=__version__,
-        )
+    author=author,
+    author_email=author_email,
+    description=description,
+    entry_points={
+        'console_scripts': [
+            'robindro = bin.robindro:run_robindro',
+        ]
+    },
+    include_package_data=True,
+    install_requires=install_requires,
+    keywords=keywords,
+    license='GPL',
+    long_description=long_description,
+    name='Robindro',
+    packages=(
+        'robindro',
+    ),
+    platforms='Any',
+    url='http://github.com/execat/robindro',
+    version=__version__,
+)
