f0c5fb33550462e1b453c72bf885e167e12c856c
Anuj More <anujmorex@gmail.com>
2015-01-30 01:53:49 +0530

    Better versions file

--- robindro/_version.py
+++ robindro/_version.py
@@ -1,9 +1,20 @@
 # Copyright (c) Anuj More
-# See LICENSE for details
+# See LICENSE.md for details
 
 """
 Provides Robindro version information
 """
 
-__version_info__ = (0, 1)
-__version__ = '.'.join(str(x) for x in __version_info__)
+# Version is a human-readable version number. Use http://semver.org/ to decide
+# a proper version while editing the version number
+
+def version_string(info):
+    import code
+    code.interact(local=locals())
+    product_version = '.'.join(info[0:3])
+    if len(info) == 4:
+        return product_version + "-" + info[3]
+    return product_version
+
+__version_info__ = ('0', '1', '0', 'alpha.1')
+__version__ = version_string(__version_info__)
