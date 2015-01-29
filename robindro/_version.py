aa469a71baa37a8f9442496ba59d77bcce6bd769
Anuj More <anujmorex@gmail.com>
2015-01-30 00:24:20 +0530

    (wip)

--- robindro/_version.py
+++ robindro/_version.py
@@ -1,2 +1,9 @@
+# Copyright (c) Anuj More
+# See LICENSE for details
+
+"""
+Provides Robindro version information
+"""
+
 __version_info__ = (0, 1)
 __version__ = '.'.join(str(x) for x in __version_info__)
