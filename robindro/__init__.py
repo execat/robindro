3fe9f2a87387df5a1eb037effa8372447bd8e9ef
Anuj More <anujmorex@gmail.com>
2015-02-01 11:16:23 +0530

    Edit version, adds util filesg

---
+++ robindro/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from .compat import * # FIX THIS AFTER KNOWING WHAT IS NEEDED
---
+++ robindro/version.py
@@ -0,0 +1,2 @@
+from __future__ import unicode_literals
+__version__ = '2015.1.31a1'
